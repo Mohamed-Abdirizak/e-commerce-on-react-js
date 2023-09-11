ea301bf19d8d4c245d52d728c22e8a862808d77d
Fatih_Abdirizak_Ahmed <fatihabdi2000@gmail.com>
2023-09-11 08:23:33 +0300

    navigation section done

--- src/navigations/Nav.js
+++ src/navigations/Nav.js
@@ -1,12 +1,33 @@
+import {FiHeart} from 'react-icons/fi'
+import {AiOutlineAccountBook, AiOutlineShoppingCart, AiOutlineUserAdd} from 'react-icons/ai'
+
 import './Nav.css'
 import React from 'react'
 
  function Navigation() {
   return (
-    <div>
-        Nav
+    <nav>
+      <div className='nav-container'>
+        <input type='text' className='search-input' placeholder='Enter your search shoes.' />
+  
+      </div>
+
+      <div className='profile-container'>
+        <a href='#' >
+          <FiHeart className='nav-icons' />
+        </a>
+
+        <a href='#' >
+          <AiOutlineShoppingCart className='nav-icons' />
+        </a>
+        <a href='#' >
+          <AiOutlineUserAdd className='nav-icons' />
+        </a>
+
+      </div>
+        
       
-    </div>
+    </nav>
   )
 }
 
